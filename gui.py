f4987268204c5c257d3a11ce9b666aa8d3f7220d
Chinmay Bhole <chinmaymotog5@gmail.com>
2022-03-05 17:38:19 +0530

    first commit

---
+++ gui.py
@@ -0,0 +1,126 @@
+#Creating GUI with tkinter
+import tkinter
+from tkinter import *
+from bot import chatbot_response,displaycsv,room_func
+from PIL import ImageTk, Image
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+
+def get_div(msg_lst,msg):
+    for i in msg_lst:
+        if i[0]== 'd':            
+            return i.upper()
+    else:
+        ChatLog.config(state=NORMAL)
+        ChatLog.insert(END, "You: " + msg + '\n\n')
+        ChatLog.config(foreground="#442265", font=("Verdana", 12 ))
+        print('Please also provide division')
+        ChatLog.insert(END, "Bot: " + 'Div Not found' + '\n\n')
+
+def timetable(div):
+    div = div.upper()
+    win = tkinter.Toplevel()
+    win.geometry("400x400")
+    p1 = PhotoImage(file = 'veslogo.png')
+    win.iconphoto(False, p1)
+    win.title(f"TimeTable of DIV {div}")
+    img = ImageTk.PhotoImage(Image.open(f'Timetable/{div}.jpg'))
+    Label(win,image = img).pack()
+    print("TimeTable")
+    win.mainloop()
+
+def send():
+    msg = EntryBox.get("1.0",'end-1c').strip()
+    msg_lst = word_tokenize(msg)
+    msg_lst = [i.lower() for i in msg_lst]
+    # msg_lst = [lemmatizer.lemmatize(word.lower()) for word in msg_lst]
+    print(msg_lst)
+    EntryBox.delete("0.0",END)
+    df = displaycsv()
+    res = chatbot_response(msg)
+    ChatLog.config(state=NORMAL)
+    
+
+    if "Timetable".lower() in msg_lst:
+
+        ChatLog.config(state=NORMAL)
+        ChatLog.insert(END, "You: " + msg + '\n\n')
+        ChatLog.config(foreground="#442265", font=("Verdana", 12 ))
+        # ChatLog.insert(END, "You: " + msg + '\n\n')
+        # ChatLog.insert(END, "You: " + msg + '\n\n')
+        timetable(get_div(msg_lst,msg))
+    
+    if res == "Room_func":
+        room,flag = room_func(msg,df)
+        print(room,flag)
+        # ChatLog.config(state=NORMAL)
+        if flag == True:
+            ChatLog.insert(END, "You: " + msg + '\n\n')
+            ChatLog.insert(END, "Bot: " + room + '\n\n')
+            ChatLog.config(foreground="#442265", font=("Verdana", 12 ))
+        elif flag == False:
+            ChatLog.insert(END, "You: " + msg + '\n\n')
+            ChatLog.insert(END, "Bot: " +"Oops !!This professor name is not present in this department\nBelow is the list of Professors:"+ '\n\n')
+            ChatLog.insert(END,df["Professors"])
+            ChatLog.config(foreground="#442265", font=("Verdana", 12 ))
+
+        
+    elif msg != " ":
+        # ChatLog.config(state=NORMAL)
+        ChatLog.insert(END, "You: " + msg + '\n\n')
+        # print("LALALALALALALALAaaaaaa")
+        ChatLog.config(foreground="#442265", font=("Verdana", 12 ))
+
+        # res = chatbot_response(msg)
+        print(res)
+        if res == "PROF_CSV":
+                ChatLog.insert(END,df)
+        else:
+            ChatLog.insert(END, "Bot: " + res + '\n\n')
+            # print("ABRACADABRA")
+
+
+        ChatLog.config(state=DISABLED)
+        ChatLog.yview(END)
+
+
+root = Tk()
+root.title("Enquiry-Bot")
+root.geometry("600x500")
+root.resizable(False, False)
+p1 = PhotoImage(file = 'veslogo.png')
+# p2 = ImageTk.PhotoImage(Image.open('Timetable/D16B.jpg'))
+# label = Label(root,image = p2)
+root.iconphoto(False, p1)
+root.resizable(width=True, height=FALSE)
+
+#Create Chat window
+ChatLog = Text(root, bd=0, bg="white", height="8", width="50", font="Arial",)
+ChatLog.config(state=DISABLED)
+
+#Bind scrollbar to Chat window
+scrollbar = Scrollbar(root, command=ChatLog.yview, cursor="heart")
+# scrollbar = Scrollbar(root, command=p2.yview, cursor="heart")
+ChatLog['yscrollcommand'] = scrollbar.set
+# p2['yscrollcommand'] = scrollbar.set
+
+#Create Button to send message
+SendButton = Button(root, font=("Verdana",12,'bold'), text="Send", width="12", height=2,
+                    bd=0, bg="#32de97", activebackground="#3c9d9b",fg='#ffffff',
+                    command= send )
+
+#Create the box to enter message
+EntryBox = Text(root, bd=0, bg="white",width="29", height="2", font="Arial")
+#EntryBox.bind("<Return>", send)
+
+
+#Place all components on the screen
+# label.place(x=600,y=6,height=570)
+# scrollbar.place(x=1252,y=6, height=570)
+scrollbar.place(x=576,y=6, height=386)
+ChatLog.place(x=6,y=6, height=386, width=570)
+EntryBox.place(x=128, y=401, height=90, width=465)
+SendButton.place(x=6, y=401, height=90)
+
+root.mainloop()
---
+++ training.py
@@ -0,0 +1,95 @@
+from copyreg import pickle
+import random
+from pprint import pprint
+import pickle
+import json
+import numpy as np
+
+import nltk
+# nltk.download('omw-1.4')
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense,Activation,Dropout
+from tensorflow.keras.optimizers import SGD,Adam
+from tensorflow.keras import callbacks
+
+intents = json.loads(open('C:/Users/91703/Desktop/projects/Enquiry-Chatbot/intents.json').read())
+# print(intents['intents'])
+
+lemmatizer = WordNetLemmatizer() # It is a technique use in nlp, basically convert a word to lemma i.e. the simmplest meaningful form of that word
+
+words = [] # list of list of different words
+classes = []
+documents = [] # list of words and there respective tag in a tuple
+ignore_letters = ['?','.','!',',']
+
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+    # for pattern in intent['pattern']:
+        word_list = word_tokenize(pattern)
+        words.extend(word_list)
+        documents.append((word_list,intent['tag']))
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+
+words = [lemmatizer.lemmatize(word) for word in words if word not in ignore_letters]
+words = sorted(set(words))
+classes = sorted(set(classes))
+
+print(classes)
+# Convert to  pickle file
+pickle.dump(words,open('words.pkl','wb'))
+pickle.dump(classes,open('classes.pkl','wb'))
+
+
+###################################################################################################################################
+# This entire process is there so that the neural network can have a array as an input because it will not take words as input
+training = []
+output_empty = [0]*len(classes) 
+for document in documents:
+    bag = []
+    word_patterns = document[0]
+    word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
+    for word in words:
+        bag.append(1) if word in word_patterns else bag.append(0)
+    # print(bag)
+
+    output_row = list(output_empty)# This method is used to copy the list
+    # print('empty output list: ',output_empty)
+    # print('copy of empty list',output_row)
+    # print('index: ',classes.index(document[1]))
+    output_row[classes.index(document[1])] = 1
+ 
+    training.append([bag,output_row])
+###################################################################################################################################
+# print(training)
+random.shuffle(training)
+training= np.array(training)
+
+train_x = list(training[ : , 0])
+train_y = list(training[ : , 1])
+# print(train_y)
+# # Builing Model 
+act = 'tanh'
+stop = callbacks.EarlyStopping(monitor='val_loss',patience=10)
+model = Sequential()
+model.add(Dense(100,input_shape = (len(train_x[0]),),activation=act)) # input_shape parameter adds a layer before current layer as a input layer
+model.add(Dropout(0.15)) 
+model.add(Dense(100,activation=act))
+model.add(Dropout(0.15)) 
+model.add(Dense(50,activation=act))
+model.add(Dropout(0.15)) 
+model.add(Dense(25,activation=act))
+model.add(Dense(len(train_y[0]),activation='softmax')) # Output layer
+# model.add(Dense(len(train_y[0]),activation='sigmoid'))
+sgd = SGD(learning_rate=0.01,momentum=0.9,nesterov=True) # sgd optimiser is giving an error during predict operation ([nan nan])
+adam = Adam(learning_rate=0.001)
+model.compile(loss = 'categorical_crossentropy',optimizer=adam,metrics=['accuracy'])
+# model.compile(loss = 'binary_crossentropy',optimizer=adam,metrics=['accuracy'])
+
+model.fit(np.array(train_x), np.array(train_y), epochs= 200,batch_size=4,verbose=1,callbacks=[stop],validation_split=0.15)
+# model.save('datazen1.h5')
+model.save('aibotmodelprof.h5')
+print('~~~~~ DONE ! ~~~~~~')
